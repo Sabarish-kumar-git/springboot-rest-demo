e2b99cf06d8d4d685ad13d977092d04c53273d79
Sabarish <sabarishkumar.r@gmail.com>
2025-07-04 20:39:16 +0530

    changes commit

--- springboot-rest-demo/src/main/java/com/example/springboot_rest_demo/TestController.java
+++ springboot-rest-demo/src/main/java/com/example/springboot_rest_demo/TestController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    /**
+     * A simple endpoint that returns a greeting message.
+     * @return String greeting message
+     */
     @GetMapping("/greet")
     public String greet() {
         return "Hello, welcome to the Spring Boot REST demo!";

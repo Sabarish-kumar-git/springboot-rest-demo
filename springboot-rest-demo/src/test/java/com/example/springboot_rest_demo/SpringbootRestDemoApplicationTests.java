3ebeaabc68b675e092c813fd3dad288a71ca60b0
Sabarish <sabarishkumar.r@gmail.com>
2025-07-05 16:39:37 +0530

    Committed for test class comment changes

--- springboot-rest-demo/src/test/java/com/example/springboot_rest_demo/SpringbootRestDemoApplicationTests.java
+++ springboot-rest-demo/src/test/java/com/example/springboot_rest_demo/SpringbootRestDemoApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.springboot_rest_demo;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootRestDemoApplicationTests {
+	public static Logger logger = LoggerFactory.getLogger(SpringbootRestDemoApplicationTests.class);
 
 	@Test
 	void contextLoads() {
+		logger.info("Context loads test executed successfully.");
 	}
 
 }
